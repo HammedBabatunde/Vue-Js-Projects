cff9fe24f8a59bd67abc81cb020f791d4d731c4e
HammedBabatunde <olawale6708@gmail.com>
2023-09-02 20:39:19 +0100

    Nodejs tutorial

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
+
+app.use(bodyParser.json())
 app.use('/', express.static(__dirname + '/'))
 
 app.post('/save-task', function(req, res) {
     const taskObj = req.body
     console.log(taskObj.task)
-
+    //connect database
+    // saves new task in database
+    console.log("saved Task: ", taskObj.task)
     res.send({savedTask: taskObj.task})
+})
+
+
+app.get('/get-tasks', function(req, res) {
+    const tasks = [ 
+        "Write JS code",
+        "Write HTML code",
+        "Read 10 pages",
+        "Go to the gym"
+    ]
+    res.send({tasks: tasks})
 })
 
 app.listen(3000, function () { 
