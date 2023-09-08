aaaad00cd0af1ebb184f842c84ee281c08b7a9e2
User <user@Users-MacBook-Pro.local>
2023-09-08 22:57:44 +0100

    Persisted data to db

--- server.js
+++ server.js
@@ -1,31 +1,70 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const { MongoClient } = require('mongodb')
 
+//connect to mongodb database
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+// Create database
+const dbName = 'company_db';
+const collName = 'employees';
 
 //parses payload
 app.use(bodyParser.json())
 app.use('/', express.static(__dirname + '/dist'))
 
 
-app.get('/get-profile', function(req, res) { 
+app.get('/get-profile', async function(req, res) { 
+    //connect to db
+    await client.connect()
+    console.log("Connected successfully to server")
+
+    // initiate or get the db and collection
+    const db = client.db(dbName)
+    const collection = db.collection(collName)
+
     //get data from database
-    const response = {
-        name: 'John Doe',
-        email: 'olawale6708@gmail.com',
-        interests: 'Football'
+    const result = await collection.findOne({id: 1})
+    client.close() 
+
+    let response = {}
+
+    if (result !== null) {  
+        response = { 
+            name: result.name,
+            email: result.email,
+            interests: result.interests
+        }
     }
 
     res.send(response)
 })
 
-app.post('/update-profile', function(req, res) {
+app.post('/update-profile', async function(req, res) {
     const payload = req.body
     console.log(payload)
+
+
     if (Object.keys(payload).length ===0 ) {
         res.status(400).send({error: "empty payload. Couldnt update user profile"})
     } else {
+
+        // connect to mongodb database
+        await client.connect()
+        console.log("Connected successfully to server")
+
+        // initiate or get the db and collection
+        const db = client.db(dbName)
+        const collection = db.collection(collName)
+
         //saving payload into database
+        payload['id'] = 1;
+        const updatedValues = { $set: payload }
+        await collection.updateOne({id: 1}, updatedValues, {upsert: true})
+        client.close()
+
         res.status(200).send({info: "user profile data updated successfully"})
     }
 })
